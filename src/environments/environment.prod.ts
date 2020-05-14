f3e2f817b82599b1723a693b25b1c7fbed28546f
TarunShrivastva <shrivastvatarun@gmail.com>
2020-05-14 16:59:26 +0530

    production changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:8000/api/v1/',
-  crm_url: 'http://localhost:8000/',
-  socket_url: 'http://crm.instaveritas.app.test:3300',
-  fir_url: 'http://crm.test/api/',
-  url: 'http://localhost:4200',
-  image: 'http://localhost:8000/upload/'
+  api_url: 'http://tech4news.in/api/v1/',
+  url: 'http://tech4news.in/',
+  image: 'http://tech4news.in/upload/'
 };
